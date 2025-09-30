73f7176e3d21d81215f0a1b9928df62359b7b077
Kerin258 <kevingmp18@gmail.com>
2025-09-30 00:18:50 -0600

    Codigos Con Y Sin Hilos con el READMI

--- Java/MonteCarlo.java
+++ Java/MonteCarlo.java
@@ -1,29 +1,34 @@
-package Java;
-
-import java.util.Random; 
+import java.util.Random;
 
 public class MonteCarlo {
 
-    public  static void main (String[] args) {
-        int totalSamples = 1_000_000; //numero total de puntos
-        long insideCircle = 0; //contador total
-        Random rand = new Random();
+    public static void main(String[] args) {
+        int totalSamples = 1000000;
+        int circulo = 0;
 
-        //generar puntos aleatorios y verificar si caen dentro del circulo
-        for(int i = 0; i < totalSamples; i++) {
-            double x = rand.nextDouble();
-            double y = rand.nextDouble();
-            if(x * x + y * y <= 1.0) {
-                insideCircle++;
+        Random random = new Random();
+
+        long tiempoInicio = System.currentTimeMillis();
+
+        for (int i = 0; i < totalSamples; i++) {
+            double x = random.nextDouble(); // número en [0,1)
+            double y = random.nextDouble(); // número en [0,1)
+
+            if (x * x + y * y <= 1.0) {
+                circulo++;
             }
         }
 
-        //calcular aprox. de pi
-        double piApprox = (4.0 * insideCircle) / totalSamples;
-        System.out.println("Numero total de puntos: " + totalSamples);
-        System.out.println("Puntos dentro de circulo: " + insideCircle);
-        System.out.println("Aproximacion de pi: " + piApprox);
+        long tiempoFinal = System.currentTimeMillis();
+
+        double piApprox = 4.0 * circulo / totalSamples;
+
+        System.out.println("Número total de puntos: " + totalSamples);
+        System.out.println("Puntos dentro del círculo: " + circulo);
+        System.out.println("Aproximación de pi: " + piApprox);
         System.out.println("Error: " + Math.abs(piApprox - Math.PI));
+        System.out.println("Tiempo total de cálculo: " + (tiempoFinal - tiempoInicio) + " ms");
     }
 }
 
+//KERIN DEL JESUS GONZALEZ MAAS
